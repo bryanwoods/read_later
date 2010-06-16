958894d3d325a9973aab2d73afd90b30e17232f1
Bryan Woods <bryanwoods4e@gmail.com>
2010-06-16 11:33:01 -0400

    Updating Example

--- examples/application.rb
+++ examples/application.rb
@@ -6,15 +6,15 @@ get '/' do
   read_later = ReadLater.new
 
   # Application variables
-  # Example URL:
+  # URL (Required):
   # h post_path(@post)
   url = "http://writing.bryanwoods4e.com/1-poor-poor-child"
 
-  # Example title:
+  # Title (Optional, but preferred):
   # h @post.title
   title = "Poor, poor child. You have no idea."
 
-  # Can customize the following iframe parameters:
+  # Can customize the following optional iframe parameters:
   # Border, scrolling, width, height, transparency,
   # And frameborder. For example:
   read_later.height = 100
@@ -28,4 +28,3 @@ get '/' do
   # <%= @instapaper_button %>
   @instapaper_button
 end
-
